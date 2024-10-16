c97b1b36c35791b71137f81bd0cd3b52ac35ede6
Muh. Naufal Bimasbuqin <mnaufalbmsbqn@gmail.com>
2024-10-17 04:53:36 +0800

    ganjil_genap.py

--- ganjil_genap.py
+++ ganjil_genap.py
@@ -1,5 +1,5 @@
 bilangan = int(input("Masukkan sebuah bilangan: "))
 if bilangan % 2 == 0:
-    print("{bilangan} adalah bilangan genap")
+    print(bilangan, "adalah bilangan genap")
 else:
-    print("{bilangan} adalah bilangan ganjil")
+    print(bilangan, "adalah bilangan ganjil")
